19248e18c8a025337724b9ff2ef48095a324f796
Alina Vinogradova <jsemaljaa@gmail.com>
2023-11-27 04:35:59 +0100

    another fix

--- iotmanagement/wsgi.py
+++ iotmanagement/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aaa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iotmanagement.settings')
 
 application = get_wsgi_application()
